b4d2b60a7833d7305b1bb607f8e3dae1fba07224
Ismael <kismaelkemal@gmail.com>
2024-04-12 21:46:14 +0300

    refactor: Update bot entry point to improve code organization and readability

--- src/bot.ts
+++ src/bot.ts
@@ -2,38 +2,98 @@ import { config } from "dotenv";
 import { Bot } from "grammy";
 import { startCommand } from "./commands/start";
 import { helpCommand } from "./commands/help";
+import { payCommand } from "./commands/pay";
 import { handleCallbackQuery } from "./callbacks";
 import { handleMessage } from "./messages";
 
-config(); // Load the environment variables from the .env file
+// Load the environment variables from the .env file
+config();
 
-// Ensure the bot token is correctly set in the environment variables
-if (!process.env.TOKEN) {
+const CHAPA_TOKEN = process.env.CHAPA_TOKEN;
+const BOT_TOKEN = process.env.TOKEN;
+
+// Ensure the environment variables are set correctly
+if (!BOT_TOKEN) {
     console.error('Missing bot token in environment variables');
     process.exit(1);
 }
 
-console.log(`${process.env.TOKEN}`); // <-- this will log your bot token to the console
-// Create an instance of the `Bot` class and pass your bot token to it.
-const bot = new Bot(`${process.env.TOKEN}`); // <-- put your bot token between the ""
+if (!CHAPA_TOKEN) {
+    console.error('Missing chapa token in environment variables');
+    process.exit(1);
+}
 
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
+// Create an instance of the `Bot` class and pass your bot token to it.
+const bot = new Bot(`${BOT_TOKEN}`);
 
 // Handle the /start command.
 bot.command("start", startCommand);
+
+// Handle the /help command.
 bot.command("help", helpCommand);
 
-// Register callback query handler
-bot.on('callback_query:data', handleCallbackQuery);
+// Handle the /pay command.
+bot.command("pay", payCommand);
 
-// Handle other messages (typed-in text, media, etc.)
+
+
+// =====================================================================
+/**
+ * Register some middlewares for callback queries, 
+ * i.e. the updates that Telegram delivers to your bot when a user clicks an inline button 
+ * (that is a button under a message).
+ */
+// =====================================================================
+
+// Register middleware to handle users pressing any button your bot ever sent
+bot.on('callback_query:data', handleCallbackQuery)
+
+// Register middleware to handle users pressing buttons with that specific payload
+bot.callbackQuery('specific-button-payload', async (ctx) => { })
+
+
+
+// =====================================================================
+/**
+ * Register some middlewares that will only be executed for some specific updates, 
+ * namely those matching the provided filter query. Filter queries are a concise way 
+ * to specify which updates you are interested in.
+ * GrammY will call the listeners when users send messages to your bot.
+ */
+// =====================================================================
+
+// Handle messages (All kinds: text from keyboard, media, url...)
 bot.on("message", handleMessage);
 
+// Handle specific kinds of messages (text messages)
+bot.on("message:text", async (ctx) => {
+    await ctx.reply(`You sent a text message: ${ctx.message?.text}`);
+});
+
+// Text messages and text channel posts
+bot.on(':text', async (ctx) => {
+    await ctx.reply(`You sent a text message: ${ctx.message?.text}`);
+});
 
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
 
-// Start the bot.
+// =====================================================================
+/**
+ * Register some middlewares that will only be executed for some specific updates,
+ * namely those matching the provided filter query. Filter queries are a concise way
+ * to specify which updates you are interested in.
+ */
+// =====================================================================
+
+// Handle pre-checkout query
+bot.on("pre_checkout_query", async (ctx) => {
+    await ctx.answerPreCheckoutQuery(true, ctx.preCheckoutQuery.id);
+});
+
+
+
+/**
+ * Now that you specified how to handle messages, you can start your bot.
+ * This will connect to the Telegram servers and wait for messages.
+ */
 bot.start();
