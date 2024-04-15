2719146470f15fb43b5502ea202257018996776a
Ismael <kismaelkemal@gmail.com>
2024-04-15 15:28:45 +0300

    Update helpCommand function to fire up the 'start' trigger

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,7 +3,7 @@ import { Context, InlineKeyboard } from "grammy";
 export const helpCommand = async (ctx: Context) => {
     await ctx.reply("This is my first attempt to build a bot with grammy (grammy.dev). Will explore the possiblities here 🙂");
     const inlineKeyboard = new InlineKeyboard()
-        .text('Start', 'callback_data_1')
+        .text('Start', 'start')
 
     await ctx.reply('We hope that is clear. If so, get started', {
         reply_markup: inlineKeyboard,
